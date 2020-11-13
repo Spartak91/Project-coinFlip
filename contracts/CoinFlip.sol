9cf63375c683cff07da17fb47a9f65e551613082
Spartak91 <porin.tomljanovic@gmail.com>
2020-11-13 13:27:04 +0100

    second commit

---
+++ contracts/CoinFlip.sol
@@ -0,0 +1,48 @@
+import "./Ownable.sol";
+pragma solidity ^0.5.12;
+
+contract CoinFlip is Ownable{
+    uint public balance;
+
+    event coinFlipped(uint result);
+    event flipWon();
+    event flipLost();
+    
+    modifier validateBet(){
+        require(msg.value > 0 && msg.value <= balance);
+        _;
+    }
+
+    function flipCoin() private returns(uint) {
+        uint result = now % 2;
+        emit coinFlipped(result);
+        return result;
+    }
+
+    function settleBet(uint prediction) public payable validateBet {
+        require(prediction == 0 || prediction == 1);
+        uint outcome = flipCoin();
+        if(outcome == prediction) {
+            emit flipWon();
+            balance -= msg.value;
+            msg.sender.transfer(msg.value * 2);
+        } else {
+            emit flipLost();
+            balance += msg.value;     
+        }
+    }
+
+    function addFunds() public onlyOwner payable returns(uint) {
+        require(msg.value > 0);
+        balance += msg.value;
+        return balance;
+    }
+
+    function withdrawAll() public onlyOwner returns(uint) {
+       uint toTransfer = balance;
+       balance = 0;
+       msg.sender.transfer(toTransfer);
+       return toTransfer;
+   }
+
+}
---
+++ dapp/main.js
@@ -0,0 +1,66 @@
+var web3 = new Web3(Web3.givenProvider);
+var address;
+var contractInstance;
+
+var alert = `<div class="alert alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`
+
+$(document).ready(function() {
+    window.ethereum.enable().then(async function(accounts) {
+        contractInstance = new web3.eth.Contract(abi, "0x4797ed1C5748A61A3edf19876A16b977507782aB", {from: accounts[0]});
+        
+        console.log(contractInstance);
+       
+        contractInstance.events.allEvents()
+            .on('data', function(event){
+                if(event.event == "flipWon") {
+                    let winningAlert = $.parseHTML(alert);
+                    $(winningAlert).addClass("alert-success");
+                    $(winningAlert).prepend("<strong>Flip Won!</strong> Your winnings have been transferred.");
+                    $("#bet-alerts").prepend(winningAlert);
+                    setTimeout(() => $(winningAlert).alert('close'), 5000);
+                } else if (event.event == "flipLost") {
+                    let losingAlert = $.parseHTML(alert);
+                    $(losingAlert).addClass("alert-danger");
+                    $(losingAlert).prepend("<strong>Flip Lost</strong> Thanks for playing!");
+                    $("#bet-alerts").prepend(losingAlert);
+                    setTimeout(() => $(losingAlert).alert('close'), 5000);
+                } else if (event.event == "coinFlipped") {
+                    let flippedAlert = $.parseHTML(alert);
+                    $(flippedAlert).addClass("alert-primary");
+                    if(event.returnValues.result == "0")
+                    {
+                        $(flippedAlert).prepend("<strong>Heads</strong>");
+                    } else {
+                        $(flippedAlert).prepend("<strong>Tails</strong>");
+                    }
+                    $("#bet-alerts").prepend(flippedAlert);
+                    setTimeout(() => $(flippedAlert).alert('close'), 5000);
+                }
+            });
+    });
+
+    $("#place_bet_button").click(placeBet);
+
+});
+
+async function placeBet(){
+    var prediction = parseInt($("#prediction").val());
+    var bet = parseFloat($("#bet_input").val()) * (10 ** 18); 
+    var balance = await contractInstance.methods.balance().call();
+    balance = parseFloat(balance);
+    
+    if(balance >= bet && bet > 0) {
+        contractInstance.methods.settleBet(prediction).send({value: bet})
+    } else {
+        let warning = $.parseHTML(alert);
+        $(warning).addClass("alert-danger");
+        $(warning).prepend("Bet must be <strong>greater</strong> than 0 and <strong>less</strong> than " + balance/(10**18) + " ETH.");
+        $("#bet-alerts").prepend(warning);
+        setTimeout(() => $(warning).alert('close'), 5000);
+    }
+}
+ 
